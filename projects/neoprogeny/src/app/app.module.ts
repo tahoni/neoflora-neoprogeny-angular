5bb783cabd269d36f47847e9116c1e5cf6c4c857
Tahoni <tahoni@gmail.com>
2020-06-17 09:26:00 +0200

    Move the image components to their own folder

--- projects/neoprogeny/src/app/app.module.ts
+++ projects/neoprogeny/src/app/app.module.ts
@@ -16,10 +16,10 @@ import {ContentComponent} from './content/content.component';
 
 import {LookupComponent} from './lookup/lookup.component';
 import {SearchComponent} from './search/search.component';
-import {ImageComponent} from './image/image.component';
-import {ImageDetailComponent} from './image-detail/image-detail.component';
+import {ImageComponent} from "./image/image/image.component";
+import {ImageDetailComponent} from './image/image-detail/image-detail.component';
 import {FileUploaderComponent} from './file-uploader/file-uploader.component';
-import {ImageUploaderComponent} from './image-uploader/image-uploader.component';
+import {ImageUploaderComponent} from './image/image-uploader/image-uploader.component';
 
 import {HybridNewComponent} from './hybrid/hybrid-new/hybrid-new.component';
 import {HybridSearchSimpleComponent} from './hybrid/hybrid-search/hybrid-search-simple/hybrid-search-simple.component';
