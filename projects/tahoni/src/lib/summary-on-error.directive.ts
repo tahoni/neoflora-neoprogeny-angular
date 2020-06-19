f3178fb83df0dfce7c883c556b91b54227a42521
Tahoni <tahoni@gmail.com>
2020-06-19 11:57:55 +0200

    Added three directives

---
+++ projects/tahoni/src/lib/summary-on-error.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[libSummaryOnError]'
+})
+export class SummaryOnErrorDirective {
+
+  constructor() { }
+
+}
--- projects/tahoni/src/lib/tahoni.module.ts
+++ projects/tahoni/src/lib/tahoni.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { TahoniComponent } from './tahoni.component';
+import { TouchOnErrorDirective } from './touch-on-error.directive';
+import { FocusOnErrorDirective } from './focus-on-error.directive';
+import { SummaryOnErrorDirective } from './summary-on-error.directive';
 
 
 
 @NgModule({
-  declarations: [TahoniComponent],
+  declarations: [TahoniComponent, TouchOnErrorDirective, FocusOnErrorDirective, SummaryOnErrorDirective],
   imports: [
   ],
   exports: [TahoniComponent]
