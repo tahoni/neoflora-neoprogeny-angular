0909c8c6c6535f8227a4a13a9932ac303d06cad7
Tahoni <tahoni@gmail.com>
2020-06-19 21:03:44 +0200

    Synced different projects

--- projects/eneoflora/src/app/app.module.ts
+++ projects/eneoflora/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
-import { ContentComponent } from './content/content.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './content/header/header.component';
+import {FooterComponent} from './content/footer/footer.component';
+import {BodyComponent} from './body/body.component';
+import {ContentComponent} from './content/content.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +14,15 @@ import { ContentComponent } from './content/content.component';
     HeaderComponent,
     FooterComponent,
     BodyComponent,
-    ContentComponent
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- projects/eneoflora/src/app/content/footer/footer.component.ts
+++ projects/eneoflora/src/app/content/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IconService} from "../../shared/icon.service";
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public iconService: IconService) {
+  }
 
   ngOnInit(): void {
   }
