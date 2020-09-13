7eba8456bd75c4c5495a541536ab63d6067162ad
Leoni Lubbinge <tahoni@gmail.com>
2020-09-13 23:20:05 +0200

    Added an image overlay component

---
+++ projects/neoprogeny/src/app/image/image-overlay/image-overlay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-overlay',
+  templateUrl: './image-overlay.component.html',
+  styleUrls: ['./image-overlay.component.css']
+})
+export class ImageOverlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
