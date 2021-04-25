e236275564b67c92e61f67b075098e5218a037fc
tahoni <tahoni@gmail.com>
2021-04-25 02:55:39 +0200

    Fix routing config

--- projects/neoprogeny/src/app/app-routing.module.ts
+++ projects/neoprogeny/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(routes)
   ],
   exports: [
     RouterModule
