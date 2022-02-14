236412d4657364df2294b1ed88851c959a098cd5
tahoni <tahoni@gmail.com>
2022-02-14 18:53:14 +0200

    Neoflora project sharp

--- projects/e-neoflora/src/app/app.component.spec.ts
+++ projects/e-neoflora/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eneoflora'`, () => {
+  it(`should have as title 'e-neoflora'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('eneoflora');
+    expect(app.title).toEqual('e-neoflora');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('eneoflora app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('e-neoflora app is running!');
   });
 });
