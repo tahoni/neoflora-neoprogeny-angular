e3683d994fe29244d582a5b18423408a09c5c85a
tahoni <tahoni@gmail.com>
2022-02-14 22:35:15 +0200

    e-Neoflora working

--- projects/e-neoflora/src/app/app.module.ts
+++ projects/e-neoflora/src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NeofloraLibModule} from "@tahoni/neoflora-lib";
+import {BodyComponent} from "./form/body/body.component";
+import {ContentComponent} from "./form/content/content.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NeofloraLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
