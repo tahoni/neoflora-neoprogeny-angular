30e50ccae5ba6aa058f17e9a0c523ad4fbd6f686
Leoni Lubbinge <tahoni@gmail.com>
2023-04-11 20:18:43 +0200

    Add the HTTP client module

--- projects/neoprogeny/src/app/app.module.ts
+++ projects/neoprogeny/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+
+import {NeofloraLibModule} from "@tahoni/neoflora-lib";
+import {TahoniLibModule} from "@tahoni/tahoni-lib";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BodyComponent} from './form/body/body.component';
 import {ContentComponent} from './form/content/content.component';
-import {NeofloraLibModule} from "@tahoni/neoflora-lib";
 import {HybridModule} from "./hybrid/hybrid.module";
-import {TahoniLibModule} from "@tahoni/tahoni-lib";
-import { ImageComponent } from './image/image.component';
-import { SearchComponent } from './search/search.component';
+import {ImageComponent} from './image/image.component';
+import {SearchComponent} from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { SearchComponent } from './search/search.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TahoniLibModule,
     NeofloraLibModule,
