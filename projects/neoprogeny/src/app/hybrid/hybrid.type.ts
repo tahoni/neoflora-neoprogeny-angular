926e15934cadc5562842dc3cfb7f1d215c83716f
Leoni Lubbinge <tahoni@gmail.com>
2023-04-13 09:19:29 +0200

    Change the constructor of a hybrid type to an object

--- projects/neoprogeny/src/app/hybrid/hybrid.type.ts
+++ projects/neoprogeny/src/app/hybrid/hybrid.type.ts
@@ -5,26 +5,28 @@ import {Offspring} from "../types/offspring.type";
 export class Hybrid {
   id: number;
   code: string;
-  mother: Hybrid | null;
-  father: Hybrid | null;
-  description: string | null;
-  seed: Seed | null;
-  offspring: Offspring | null;
-  photo: Photo | null;
-  comment: string | null;
+  mother?: Hybrid | null;
+  father?: Hybrid | null;
+  description?: string | null;
+  seed?: Seed | null;
+  offspring?: Offspring | null;
+  photo?: Photo | null;
+  comments?: string | null;
 
-  constructor(id: number, code: string, mother: Hybrid | null = null, father: Hybrid | null = null,
-              description: string | null = null, seed: Seed | null = null, offspring: Offspring | null = null,
-              photo: Photo | null = null, comment: string | null = null) {
-    this.id = id;
-    this.code = code;
-    this.mother = mother;
-    this.father = father;
-    this.description = description;
-    this.seed = seed;
-    this.offspring = offspring;
-    this.photo = photo;
-    this.comment = comment;
+  constructor(hybrid: {id: number, code: string, mother?: Hybrid | null, father?: Hybrid | null,
+              description?: string | null, seed?: Seed | null, offspring?: Offspring | null,
+              photos?: Photo[] | null, comments?: string | null}) {
+    this.id = hybrid.id;
+    this.code = hybrid.code;
+    this.mother = hybrid.mother;
+    this.father = hybrid.father;
+    this.description = hybrid.description;
+    this.seed = hybrid.seed;
+    this.offspring = hybrid.offspring;
+    if ((hybrid.photos) && (hybrid.photos.length > 0)) {
+      this.photo = hybrid.photos[0];
+    }
+    this.comments = hybrid.comments;
   }
 
 }
